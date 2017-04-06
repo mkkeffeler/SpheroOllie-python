5273c1f4896f19af8576e1fb32ef0c2567b726c3
Miclain Keffeler <mkkeffeler@crimson.ua.edu>
2017-04-06 11:43:51 -0500

    Add ability to turn via i,j,k and l to Ollietest

--- Ollietest.py
+++ Ollietest.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-from bluepy import btle
 import struct
 import time
 import Ollie_driver
@@ -16,7 +15,20 @@ ollie.set_rgb_led(0,255,0,0,False)
 time.sleep(1)
 ollie.set_rgb_led(0,0,255,0,False)
 time.sleep(3)
+ollie.set_stablization(False,'False')
+while 1:
+	key = raw_input("what is your character")
+	if(key == "l"):
+		ollie.set_raw_motor_values(1,60,1,40,'01h')
+	if(key== "j"):
+		ollie.set_raw_motor_values(1,40,1,60,'01h')
+	if(key== "i"):
+		ollie.set_raw_motor_values(1,60,1,60,'01h')
+	if(key == "k"):
+		ollie.set_raw_motor_values(2,60,2,60,'01h')
+	if(key=='g'):
+		break
+time.sleep(5)
 ollie.join()
-ollie.disconnect()
 sys.exit(1)
 
